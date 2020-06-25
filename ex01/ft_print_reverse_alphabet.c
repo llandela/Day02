88539c4a974f51352987603f62fc417e0b859ec9
llandela <66991825+llandela@users.noreply.github.com>
2020-06-25 15:27:18 +0200

    Add files via upload

--- ex01/ft_print_reverse_alphabet.c
+++ ex01/ft_print_reverse_alphabet.c
@@ -6,7 +6,7 @@
 /*   By: llandela <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/19 12:10:28 by llandela          #+#    #+#             */
-/*   Updated: 2020/06/19 15:55:59 by llandela         ###   ########.fr       */
+/*   Updated: 2020/06/25 15:26:07 by llandela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-void	ft_print_reverse_alphabet()
+void	ft_print_reverse_alphabet(void)
 {
 	char i;
 	
